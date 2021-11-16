67e5cc0687b46b815ccf183f94910f9ad680f74b
Guido22 <GuidoDulos@valori.nl>
2021-11-16 18:47:17 +0100

    Versie 12 optie flipkaart erbij

--- src/main/java/KaartenGui.java
+++ src/main/java/KaartenGui.java
@@ -29,7 +29,7 @@ public class KaartenGui extends JFrame {
         beheerview.createGuiBeheer();
         leersessieschrijven.createGuiLeersessie();
         setUpTabs();
-        setTitle("Leren met flashcards -15-11-2021- Guido Dulos  versie 11");
+        setTitle("Leren met flashcards -16-11-2021- Guido Dulos  versie 12");
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         repaint();
